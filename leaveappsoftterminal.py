393ff0f8189b60fdf8dcac530570f45bff5c1031
fardousks97 <iamhasanalfardous@gmail.com>
2021-09-12 13:22:50 +0600

    feat: add while loop, contunue and break statment.

--- leaveappsoftterminal.py
+++ leaveappsoftterminal.py
@@ -1,17 +1,19 @@
 print('Onudevs Leave Application System')
 
-employeename = input('User Name:')
-employeeid = input('ID:')
-
-if employeename == 'Fardous' and employeeid == '1004':
-	print('log in successfull')
-	print('What type of leave you need')
-	leavestype = ['Annual Leave', 'Sick Leave', 'Casual Leave']
-	print(leavestype)
-	leavetype = input('Leave:')
-	days= input('How many days:')
-	date= input('Date:')
-	print('Thanks for your application. Soon HR will confirm you through mail') 
-else:
-	print(' Your user Name or Id is Invalid')
+while True:	
+	employeename = input('User Name:')
+	employeeid = input('ID:')
+	if employeename == 'Fardous' and employeeid == '1004':
+		print('log in successfull')
+		print('What type of leave you need')
+		leavestype = ['Annual Leave', 'Sick Leave', 'Casual Leave']
+		print(leavestype)
+		leavetype = input('Leave:')
+		days= input('How many days:')
+		date= input('Date:')
+		print('Thanks for your application. Soon HR will confirm you through mail') 
+		break
+	else:
+		print(' Your user Name or Id is Invalid')
+		continue
 
