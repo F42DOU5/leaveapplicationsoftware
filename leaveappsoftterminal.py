0ab8e87184bc29f3fe48626907e8841f324fb7a3
fardousks97 <iamhasanalfardous@gmail.com>
2021-09-12 14:02:44 +0600

    feat: add multiple user using elif statment for experiment

--- leaveappsoftterminal.py
+++ leaveappsoftterminal.py
@@ -12,6 +12,24 @@ while True:
 		days= input('How many days:')
 		date= input('Date:')
 		print('Thanks for your application. Soon HR will confirm you through mail') 
+	elif employeename == 'Ratia' and employeeid == '1008':
+		print('log in successfull')
+		print('What type of leave you need')
+		leavestype = ['Annual Leave', 'Sick Leave', 'Casual Leave']
+		print(leavestype)
+		leavetype = input('Leave:')
+		days= input('How many days:')
+		date= input('Date:')
+		print('Thanks for your application. Soon HR will confirm you through mail') 
+	elif employeename == 'Sayanta' and employeeid == '1009':
+		print('log in successfull')
+		print('What type of leave you need')
+		leavestype = ['Annual Leave', 'Sick Leave', 'Casual Leave']
+		print(leavestype)
+		leavetype = input('Leave:')
+		days= input('How many days:')
+		date= input('Date:')
+		print('Thanks for your application. Soon HR will confirm you through mail') 
 		break
 	else:
 		print(' Your user Name or Id is Invalid')
