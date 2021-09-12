5e6f90c1596262884901dcd4a42319b1186ae8f3
fardousks97 <iamhasanalfardous@gmail.com>
2021-09-12 16:05:04 +0600

    feat: add employeedatabase and employeeidatabase in list

--- leaveappsoftterminal.py
+++ leaveappsoftterminal.py
@@ -1,9 +1,14 @@
 print('Onudevs Leave Application System')
 
 while True:	
+	employeenameDatabase = ['Fardous','Ratia','Sayanta','Rafayet','Choyon','Riffat','Wahid','Rituparna']
+	employeeidDatabase = ['1004','1008','1009','1010','1011','1013','1014','1015' ]	
+
 	employeename = input('User Name:')
 	employeeid = input('ID:')
-	if employeename == 'Fardous' and employeeid == '1004':
+
+	
+	if employeename in employeenameDatabase and employeeid in employeeidDatabase:
 		print('log in successfull')
 		print('What type of leave you need')
 		leavestype = ['Annual Leave', 'Sick Leave', 'Casual Leave']
@@ -11,25 +16,7 @@ while True:
 		leavetype = input('Leave:')
 		days= input('How many days:')
 		date= input('Date:')
-		print('Thanks for your application. Soon HR will confirm you through mail') 
-	elif employeename == 'Ratia' and employeeid == '1008':
-		print('log in successfull')
-		print('What type of leave you need')
-		leavestype = ['Annual Leave', 'Sick Leave', 'Casual Leave']
-		print(leavestype)
-		leavetype = input('Leave:')
-		days= input('How many days:')
-		date= input('Date:')
-		print('Thanks for your application. Soon HR will confirm you through mail') 
-	elif employeename == 'Sayanta' and employeeid == '1009':
-		print('log in successfull')
-		print('What type of leave you need')
-		leavestype = ['Annual Leave', 'Sick Leave', 'Casual Leave']
-		print(leavestype)
-		leavetype = input('Leave:')
-		days= input('How many days:')
-		date= input('Date:')
-		print('Thanks for your application. Soon HR will confirm you through mail') 
+		print('Thanks for your application. Soon HR will confirm you through mail.') 
 		break
 	else:
 		print(' Your user Name or Id is Invalid')
