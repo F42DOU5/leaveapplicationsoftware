acf35b4062a7ab70820543fdf537c6ef7c647de6
fardousks97 <iamhasanalfardous@gmail.com>
2021-09-19 00:49:00 +0600

    fix: remove added if statment while loop which add previously

--- leaveappsoftterminal.py
+++ leaveappsoftterminal.py
@@ -11,23 +11,14 @@ while True:
 	if employeename in employeenameDatabase and employeeid in employeeidDatabase:
 		print('log in successfull')
 		print('What type of leave you need')
-		
-		leavesType = ['Annual Leave', 'Sick Leave', 'Casual Leave']
+		leavesType = ['Annual Leave', 'Sick Leave', 'Casual Leave']		
 		print(leavesType)
-		
-		leavetype = input('Leave:')
-		if leavetype in leavesType:
-			while True:
-				days= input('How many days:')
-				if days == '':
-					print('input your required days please')
-					continue
-			print('Dates: dd.mm.yy to dd.mm.yy')
-			date = input('Date:')
-			print('Thanks for your application. Soon HR will confirm you through mail.') 
-			break
-		else:
-			print('Please type Proper leave type.')
+		print('input your required days please')
+		days = input('How many days:')
+		print('Date:dd.mm.yy to dd.mm.yy')
+		date = input('Date:')
+		print('Thanks for your application. Soon HR will confirm you through mail.') 
+		break
 			
 	else:
 		print(' Your user Name or Id is Invalid.')
