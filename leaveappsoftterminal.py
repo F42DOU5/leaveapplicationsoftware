c9192b882563accf928fa956fe858ea74596a56f
fardousks97 <iamhasanalfardous@gmail.com>
2021-09-20 11:46:42 +0600

    feat: add while loop after leavetype input, days input and date input

--- leaveappsoftterminal.py
+++ leaveappsoftterminal.py
@@ -3,7 +3,7 @@ print('Onudevs Leave Application System')
 while True:	
 	employeenameDatabase = ['Fardous','Ratia','Sayanta','Rafayet','Choyon','Riffat','Wahid','Rituparna']
 	employeeidDatabase = ['1004','1008','1009','1010','1011','1013','1014','1015' ]	
-
+	
 	employeename = input('User Name:')
 	employeeid = input('ID:')
 
@@ -14,22 +14,22 @@ while True:
 		leavesType = ['Annual Leave', 'Sick Leave', 'Casual Leave']		
 		print(leavesType)
 		leavetype = input('Leave Type:')
-		if leavetype == '':
+		while leavetype == '':
 			print('Please fill up your leave type.')
-		else:
+			leavetype = input('Leave Type:')
+		days = input('How many days:')
+		while days == '':
+			print('Please fill up your required days.')
 			days = input('How many days:')
-			if days == '':
-				print('Please fill up your required days.')
-			else:
-				print('Date:dd.mm.yy to dd.mm.yy')
-				date = input('Date:')
-				if date == '':
-					print('Please fill up date.')
-				else:
-					print('Thanks for your application. Soon HR will confirm you through mail.') 
-					break
+		print('Date:dd.mm.yy to dd.mm.yy')
+		date = input('Date:')
+		while date == '':
+			print('Please fill up date.')
+			date = input('Date:')
+		print('Thanks for your application. Soon HR will confirm you through mail.') 
+		break
 			
 	else:
-		print(' Your user Name or Id is Invalid.')
+		print(' Your User name or Id is invalid.')
 		
 
